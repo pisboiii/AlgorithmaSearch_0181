11d8a401345c1960e1d8c8986a75ab58c33c6022
pisboiii <hfzfahrzi@gmail.com>
2024-04-03 21:10:01 -0700

    memasukan nomor di array

--- AlgorithmaSearch_0181.cpp
+++ AlgorithmaSearch_0181.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int arr[20]; //Array to be searched
-int n;      //Number of element in the array
+int n;      //Number of element in the array 
 int i;     //Index of array element
 
 void input()
