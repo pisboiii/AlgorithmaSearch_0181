00a6599e0251e674eb9abedaecd63567e550285b
pisboiii <hfzfahrzi@gmail.com>
2024-04-03 20:38:47 -0700

    Add project files.

---
+++ AlgorithmaSearch_0181.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int arr[20]; \\Array to be searched
+int n;
+int i;
+
+void input()
+{
+    while (true)
+    {
+        cout << "eneter the number of element in the array";
+        cin >> n;
+
+    }
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
