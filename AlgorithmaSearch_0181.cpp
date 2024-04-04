94306c4096b66775dff001579a6d9753a579d850
pisboiii <hfzfahrzi@gmail.com>
2024-04-03 21:11:59 -0700

    memanggil input

--- AlgorithmaSearch_0181.cpp
+++ AlgorithmaSearch_0181.cpp
@@ -62,7 +62,7 @@ void LinearSearch()
     } while ((ch == 'y') || (ch == 'Y'));
 }
 
-void dsiplay()
+void display()
 {
     cout << "Array elements are: ";
     for (i = 0; i < n; i++)
@@ -73,7 +73,7 @@ int main()
 {
     input();
     LinearSearch();
-  
+    display();
 
 }
 
