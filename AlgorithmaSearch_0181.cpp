16987b5354a4fa0e14eddda8a354b33b4a129922
pisboiii <hfzfahrzi@gmail.com>
2024-04-03 21:16:32 -0700

    menampilkan pesan jika angka tidak ketemu di array

--- AlgorithmaSearch_0181.cpp
+++ AlgorithmaSearch_0181.cpp
@@ -53,7 +53,7 @@ void LinearSearch()
             i++; //step 4
         }
         if (i == n)
-            cout << "\n" << item << "not found in the array\n";
+            cout << "\n" << item << "not found in the array\n"; 
         cout << "\nNumber of comparisons: " << ctr << endl;
 
         cout << "\nContinue search (y/n): "; 
